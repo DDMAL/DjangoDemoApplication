76a01765c8f344a03087dc6433505afaf5c5ced3
Andrew Hankinson <andrew.hankinson@gmail.com>
2014-04-27 15:02:37 -0400

    New: Updating Serializers

--- timekeeper/serializers/activity.py
+++ timekeeper/serializers/activity.py
@@ -1,6 +1,13 @@
 from timekeeper.models.activity import Activity
+from timekeeper.models.place import Place
 from rest_framework import serializers
 
+class PlaceActivitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Place
+
 class ActivitySerializer(serializers.HyperlinkedModelSerializer):
+    place = PlaceActivitySerializer()
+
     class Meta:
         model = Activity
--- timekeeper/serializers/person.py
+++ timekeeper/serializers/person.py
@@ -2,5 +2,7 @@ from timekeeper.models.person import Person
 from rest_framework import serializers
 
 class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    full_name = serializers.Field(source="full_name")
+
     class Meta:
         model = Person
