3ca1e3892c150adfb78d402111ee30b03dafd1a6
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-05-01 11:38:08 -0400

    Snippet serializer

--- codekeeper/serializers/snippet.py
+++ codekeeper/serializers/snippet.py
@@ -1,7 +1,24 @@
-from rest_framework import serializers
 from codekeeper.models.snippet import Snippet
+from codekeeper.models.tag import Tag
+from codekeeper.models.person import Person
+from rest_framework import serializers
+
+
+class CreatorSnippetSerializer(serializers.HyperlinkedModelSerializer):
+    full_name = serializers.ReadOnlyField()
+    class Meta:
+        model = Person
+        fields = ('url', 'full_name')
+
+
+class TagSnippetSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Tag
 
 
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    tags = TagSnippetSerializer(many=True)
+    creator = CreatorSnippetSerializer()
+
     class Meta:
         model = Snippet
