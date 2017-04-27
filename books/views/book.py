5f28bb69075fb5d5d15f32f9dabd4a91d0bdcd54
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-04-27 19:48:25 -0300

    New: Templates and Renderer

--- books/views/book.py
+++ books/views/book.py
@@ -6,8 +6,10 @@ from books.serializers.book import BookSerializer
 class BooksListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    template_name = "book_list.html"
 
 
 class BooksDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    template_name = "book_detail.html"
