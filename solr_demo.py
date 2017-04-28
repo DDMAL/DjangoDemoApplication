5934eeb9ec3fc657c7cc9bcdcbd0c05dcc972a9f
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-04-27 22:42:10 -0300

    New: Script demonstrating how to work with Solr

---
+++ solr_demo.py
@@ -0,0 +1,30 @@
+import pysolr
+
+
+SERVER = "http://localhost:8983/solr/booksearch"
+
+connection = pysolr.Solr(SERVER)
+
+print("Deleting all records in Solr")
+connection.delete(q="*:*")
+
+print("Indexing Demo documents in Solr")
+connection.add([{
+    "pk": 1,
+    "type": "book",
+    "title_s": "Charlotte's Web"
+},
+{
+    "pk": 1,
+    "type": "author",
+    "last_name_s": "White",
+    "first_name_s": "E.B."
+},
+{
+    "pk": 2,
+    "type": "author",
+    "last_name_s": "Rowling",
+    "first_name_s": "J.K."
+}])
+
+connection.commit()
