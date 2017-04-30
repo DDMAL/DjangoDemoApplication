4d77de93043c2afb6a71fe435628c95836bd8c11
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-04-30 09:57:55 -0300

    New: initial query and result reducers

---
+++ books/static/apps/booksearch/src/reducers/current_query.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = "";
+
+
+export default function currentQuery (state = INITIAL_STATE, action)
+{
+    switch (action.type)
+    {
+        default:
+            return state;
+    }
+}
---
+++ books/static/apps/booksearch/src/reducers/results.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = null;
+
+
+export default function results (state = INITIAL_STATE, action)
+{
+    switch (action.type)
+    {
+        default:
+            return state;
+    }
+}
