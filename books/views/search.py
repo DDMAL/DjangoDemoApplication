e77c480eb2042638c259470b23510986a8f15395
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-04-30 11:12:45 -0300

    New: Basic Search Server View

--- books/views/search.py
+++ books/views/search.py
@@ -1,9 +1,18 @@
+from django.conf import settings
 from rest_framework import generics
 from rest_framework import response
+import pysolr
 
 
 class SearchView(generics.GenericAPIView):
     template_name = "search.html"
 
     def get(self, request, *args, **kwargs):
-        return response.Response({})
+        connection = pysolr.Solr(settings.SOLR_SERVER)
+        query = request.GET.get('q', None)
+        if not query:
+            return response.Response([])
+
+        return_fields = ['title_s', 'author_s', 'first_name_s', 'last_name_s', 'type', 'pk']
+        results = connection.search(query, fl=return_fields)
+        return response.Response(results.docs)
