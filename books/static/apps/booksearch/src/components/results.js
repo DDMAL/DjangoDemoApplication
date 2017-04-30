0da8f4247a825e9d15e6e183657a94d06d26150d
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2017-04-30 10:33:38 -0300

    New: Stub out components

---
+++ books/static/apps/booksearch/src/components/results.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Results extends React.Component
+{
+    render ()
+    {
+        return (
+            <div>Results</div>
+        )
+    }
+}
+
+export default Results;
---
+++ books/static/apps/booksearch/src/components/search_bar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class SearchBar extends React.Component
+{
+    render ()
+    {
+        return (
+            <div>Search Bar</div>
+        );
+    }
+}
+
+export default SearchBar;
